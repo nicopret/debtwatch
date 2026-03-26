87d8ce98a8d5d70c33fca6fdfcf9d034a961e533
Nico Pretorius <info@nicopretorius.com>
2026-03-26 10:49:41 +0000

    update article article view component

--- src/containers/articleContainers/RelatedArticlesContainer.tsx
+++ src/containers/articleContainers/RelatedArticlesContainer.tsx
@@ -1,6 +1,7 @@
 import RelatedArticles from "@/components/ui/relatedArticlesComponent/RelatedArticles";
 import { getRelatedArticles } from "@/data/articles";
 import type { ArticleData } from "@/data/articles/articleTypes";
+import { renderArticlePreviewGraphic } from "./articlePreviewGraphicRegistry";
 
 export interface RelatedArticlesContainerProps {
   article: ArticleData;
@@ -9,13 +10,12 @@ export interface RelatedArticlesContainerProps {
 export default function RelatedArticlesContainer({
   article,
 }: RelatedArticlesContainerProps) {
-  const relatedArticles = getRelatedArticles(article).map((item) => ({
+  const relatedArticles = getRelatedArticles(article, 4).map((item) => ({
     slug: item.slug,
     title: item.header,
     tagline: item.tagline,
-    description: item.description,
+    previewGraphic: renderArticlePreviewGraphic(item.previewGraphicKey, item.date),
   }));
 
   return <RelatedArticles items={relatedArticles} />;
 }
-
